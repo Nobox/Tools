54e08853dcdd5f240ecfcca4e71ebd0749eba1bc
Antonio Rodriguez-Martinez <antoniwan@gmail.com>
2014-12-29 14:42:19 -0500

    project init

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.base')
+
+@section('title')
+    <title>{{ $pageTitle . ' | ' . trans('app.name') }}</title>
+@stop
+
+@section('stylesheets')
+    @if ('production' == $environment)
+    <link rel="stylesheet" href="{{ asset('css/styles.min.css') }}">
+    @else
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    @endif
+@stop
+
+@section('scripts-head')
+    {{-- Modernizr --}}
+    @if ('production' == $environment)
+    <script src="{{ asset('js/vendor/modernizr.js') }}"></script>
+    @else
+    <script src="{{ asset('bower_components/modernizr/modernizr.js') }}"></script>
+    @endif
+@stop
+
+@section('body')
+    @if(file_exists(public_path() . '/svg/symbols.svg'))
+        <?php include(public_path() . '/svg/symbols.svg'); ?>
+    @endif
+
+    @yield('content')
+@stop
+
+@section('scripts-body')
+    @if ('production' == $environment)
+    <script src="{{ asset('js/bundle.min.js') }}"></script>
+    @else
+    <script src="{{ asset('js/bundle.js') }}"></script>
+    @endif
+@stop
